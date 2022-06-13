977dca72d7b231e05b078f23df373ad466549dfa
kingmaker9841 <josephhernandez9841@gmail.com>
2022-06-13 20:40:11 +0545

    Initial

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
 const fs = require("fs")
 const path = require('path');
 
-const arr = []
+let arr = []
 
-function checkDir(dirPath) {
+function check(dirPath) {
   const dir = dirPath
   const directoryPath = path.join(__dirname, dir);
 
   return new Promise((res, rej) => {
     if (fs.existsSync(directoryPath)) {
-      fs.readdir(directoryPath, (err, files) => {
-        if (err) {
-          rej('Unable to scan directory: ' + err)
+      let files = fs.readdirSync(directoryPath)
+
+      files.forEach(file => {
+        let fileStat = fs.statSync(directoryPath + "/" + file).isDirectory()
+        if (!fileStat) {
+          arr.push(directoryPath + "/" + file)
+        } else {
+          res(checkDir(dir + "/" + file.toString()))
         }
-        files.forEach(function (file) {
-          if (file.substring(file.length - 3, file.length) === "txt") {
-            res(arr.push(directoryPath + "/" + file))
-          } else {
-            res(checkDir(dir + "/" + file.toString()))
-          }
-        });
       })
-    } else {
-      return res()
     }
   })
 
 }
 
-checkDir("root")
-  .then((res) => {
-    // console.log(arr)
+check("root")
+  .then(() => {
+    console.log(arr)
   })
-  .catch((err) => {
-    console.log(err)
-  })
+
 setTimeout(() => {
   console.log(arr)
-}, 500)
-
+}, 100)
